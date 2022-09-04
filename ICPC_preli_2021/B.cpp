797b41a14d22df3aaf1833f4ef8a95bee64f5da7
salimmia <46414940+salimmia@users.noreply.github.com>
2022-09-04 13:05:46 +0600

    Create B.cpp

---
+++ ICPC_preli_2021/B.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mx 200005
+#define ll long long
+#define mod 1000000007 //998244353
+
+int a[mx];
+char ch[mx];
+int n, m, ii, k;
+
+void solve()
+{
+    scanf("%d%d%d", &n, &m, &k);
+    int kk;
+
+    scanf("%d", &kk);
+
+
+    //cout << n << " " << m << " " << k <<
+
+    int x = (n + m + k);
+
+
+    if (x % 3 == 0) {
+        x = x / 3;
+
+        int dif1 = abs(n - x), dif2 = abs(m - x), dif3 = abs(k - x);
+
+        bool ok = false;
+
+        // cout << dif1 << " " << dif2 << " " << dif3 << endl;
+        // cout << "kk : " << kk << endl;
+
+        if ((dif1 % kk == 0) and (dif2 % kk) == 0 and (dif3 % kk) == 0)
+        {
+            ok = true;
+        }
+
+        if (ok)
+        {
+            printf("Peaceful\n");
+        }
+        else
+        {
+            printf("Fight\n");
+        }
+
+
+    }
+    else printf("Fight\n");
+
+    return;
+}
+
+int main()
+{
+    int t = 1;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++) {
+        printf("Case %d: ", i);
+        solve();
+    }
+    return 0;
+}
