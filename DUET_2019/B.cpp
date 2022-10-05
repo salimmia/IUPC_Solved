acd83a8b505b96659da5dd01aa346ebe62cadeeb
salimmia <46414940+salimmia@users.noreply.github.com>
2022-10-05 23:22:51 +0600

    Update B.cpp

--- DUET_2019/B.cpp
+++ DUET_2019/B.cpp
@@ -154,16 +154,94 @@ int main()
         printf("Case %d: ", i);
         solve();
     }
+
+    /*#include<bits/stdc++.h>
+using namespace std;
+
+#define mx 1000005
+#define ll long long
+#define mod 1000000007 //998244353
+
+int a[mx];
+char ch[mx];
+int n, m, ii, k;
+
+int lp[mx];
+vector<int>prime;
+vector<int>pf[mx];
+int P_idx[mx];
+priority_queue<pair<int, int>>pq[78500];
+int dp[mx];
+
+void sieve()
+{
+	int idx = 1;
+	for (int i = 2; i < mx - 3; i++)
+	{
+		P_idx[i] = idx - 1;
+		if (lp[i] == 0)
+		{
+			prime.push_back(i);
+			lp[i] = i;
+			P_idx[i] = idx++;
+		}
+		for (int j = 0; j < prime.size() && prime[j] <= lp[i] && i * prime[j] < mx - 3; j++)
+			lp[i * prime[j]] = prime[j];
+	}
+
+	for (int i = 2; i < mx - 3; i++)
+	{
+		int val = i;
+		while (val > 1)
+		{
+			int spf = lp[val];
+			while (val % spf == 0)val /= spf;
+			pf[i].emplace_back(P_idx[spf]);
+		}
+	}
 }
-/*
-0. Enough array size? Enough array size? Enough array size? Integer overflow?
 
-1. Think TWICE, Code ONCE!
-Are there any counterexamples to your algo?
+void solve()
+{
+	scanf("%d%d", &n, &k);
 
-2. Be careful about the BOUNDARIES!
-N=1? P=1? Something about 0?
+	dp[0] = -1;
 
-3. Do not make STUPID MISTAKES!
-Time complexity? Memory usage? Precision error?
-*/
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+		// cout << a[i] << " ";
+
+		dp[i] = dp[i - 1] + 1;
+
+		for (auto p : pf[a[i]]) {
+			while (!pq[p].empty() && pq[p].top().second < i - k) pq[p].pop();
+
+			if (pq[p].empty()) continue;
+
+			dp[i] = min(dp[i], -pq[p].top().first + 1);
+		}
+		for (auto p : pf[a[i]]) {
+			pq[p].push({ -dp[i], i});
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (auto p : pf[a[i]]) {
+			while (!pq[p].empty()) pq[p].pop();
+		}
+	}
+
+	printf("%d\n", dp[n]);
+}
+
+int main()
+{
+	int t = 1;
+	scanf("%d", &t);
+	sieve();
+	for (int i = 1; i <= t; i++) {
+		printf("Case %d: ", i);
+		solve();
+	}
+	return 0;
+}*/
