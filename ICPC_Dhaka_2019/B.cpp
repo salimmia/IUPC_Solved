b291c2f58da48d8381dcbce919f58364f6b92aeb
Muhammad Selim <46414940+salimmia@users.noreply.github.com>
2023-10-11 17:28:39 +0600

    Update B.cpp

--- ICPC_Dhaka_2019/B.cpp
+++ ICPC_Dhaka_2019/B.cpp
@@ -1,85 +1,100 @@
+// Salim_JnU
+
 #include<bits/stdc++.h>
 using namespace std;
 
-#define mx 200005
+#define mx 10005
 #define ll long long
 #define mod 1000000007 //998244353
 
-int a[mx];
-char ch[mx];
-ll n, m, ii, k, p, prefix[mx], dp[mx];
+
+ll dp[mx];
+ll pre[mx];
+int n, m, ii, p;
+ll k;
 
 void solve()
 {
-    scanf("%lld%lld%lld", &n, &p, &k);
-
-    for (int i = 0; i <= n; i++) prefix[i] = 0;
-
-    ll root = -1;
-
-    if (p > n) {
-        printf("Invalid\n");
+    scanf("%d%d%lld", &n, &p, &k);
+    int id = -1;
+    if (p > n)
+    {
+        printf("Case %d: Invalid\n", ++ii );
         return;
     }
 
-    for (int i = p; i <= n; i++) {
-        int st = i - p;
+    if (n - k >= p)
+    {
+        printf("Case %d: %lld %lld\n", ++ii, n - k + 1, n - k);
+        return;
+    }
 
-        ll tot = prefix[i - 1] + prefix[max(0, st - 1)];
+    for (int i = 0; i < p; i++)dp[i] = 0, pre[i] = 0;
+    for (int i = p; i <= n; i++)
+    {
+        int be = i - p;
+        ll tot = pre[i - 1] - pre[max(1, be - 1)];
 
-        dp[i] = tot + i - st;
+        dp[i] = tot + i - be;
+        pre[i] = pre[i - 1] + dp[i];
 
-        prefix[i] = prefix[i - 1] + dp[i];
-
-        if (dp[i] + n - i >= k) {
+        if (dp[i] + n - i >= k)
+        {
             k -= n - i;
-            root = i;
+            if (k == 0)
+            {
+                printf("Case %d: %d %d\n", ++ii, i + 1, i);
+                return;
+            }
+            id = i;
             break;
         }
     }
 
-    if (root == -1) {
-        printf("Invalid\n");
+    if (id == -1)
+    {
+        printf("Case %d: Invalid\n", ++ii );
         return;
     }
 
-    int nxt = root - 1;
-
-    while (k > 0) {
-        int hi = root - 1, lo = max(0LL, root - p);
-        ll now = 0;
-
-        for (int i = hi; i >= lo; i--) {
-            now = dp[i] + 1;
-
-            if (now < k) {
-                k -= now;
+    int cur = id;
+    int nxt = id - 1;
+    while (k > 0)
+    {
+        int jabe = max(0, cur - p);
+        for (int i = cur - 1; i >= jabe; i--)
+        {
+            ll tot = dp[i] + 1;
+            //cout<<cur<<" "<<i<<" "<<tot<<" "<<k<<endl;
+            if (tot < k)
+            {
+                k -= tot;
             }
-            else {
+            else
+            {
                 k--;
-                nxt = i;
-                if (k == 0) break;
-                else {
-                    root = i;
+                if (k == 0)
+                {
+                    nxt = i;
+                    break;
+                }
+                else
+                {
+                    cur = i;
                     nxt = i - 1;
                     break;
                 }
             }
         }
+
     }
-
-    printf("%lld %d\n", root, nxt);
-
-    return;
+    printf("Case %d: %d %d\n", ++ii, cur, nxt);
 }
 
 int main()
 {
     int t = 1;
     scanf("%d", &t);
-    for (int i = 1; i <= t; i++) {
-        printf("Case %d: ", i);
-        solve();
-    }
+    while (t--)solve();
     return 0;
 }
